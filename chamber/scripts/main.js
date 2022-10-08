2c1db6b70e9dbb2f683cb0eca7ccd20cf5375d31
nic-is-hoppi <nicolasmhopkinson@gmail.com>
2022-10-08 16:16:57 -0600

    Chamber Finished

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,3 +1,4 @@
+//Hamburger Button
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -6,7 +7,14 @@ function toggleMenu(){
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
-document.querySelector('#today').textContent = new Date().get();
+//Live date on header
+const daysofWeek = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const d = new Date();
+let dayofWeek = daysofWeek[d.getDay()]
+const monthsofYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let currentMonth = monthsofYear[d.getMonth()];
+document.querySelector('#today').textContent = dayofWeek +", " + d.getDate() + " " + currentMonth + " " + d.getFullYear();
 
+//Live Copyright and Updated on Footer
 document.querySelector('#currentyear').textContent = new Date().getFullYear();
 document.querySelector('#lastupdate').textContent = document.lastModified;
