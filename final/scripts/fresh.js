a808d7d35e4c9d4db7e310560d62795736eaaf92
nic-is-hoppi <nicolasmhopkinson@gmail.com>
2022-12-14 12:43:06 -0700

    try this

--- final/scripts/fresh.js
+++ final/scripts/fresh.js
@@ -27,9 +27,9 @@ fetch(requestURL)
    }
    
 //Check Input
-$("input:checkbox").click(function() {
-    let bol = $("input:checkbox:checked").length >=3;
-    $("input:checkbox").not(":checked").attr("disabled",bol);
+document.querySelector("input:checkbox").click(function() {
+    let bol = document.querySelector("input:checkbox:checked").length >=3;
+    document.querySelector("input:checkbox").not(":checked").attr("disabled",bol);
 });
 
 
